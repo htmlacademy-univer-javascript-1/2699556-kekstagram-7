cb1835202a3bf0cf6b5b4ab319c2ebb6eb6ca601
Алёна Западня <jczapadn@mail.ru>
2026-09-24 20:09:38 +0500

    исправлена ошибка

--- js/functions.js
+++ js/functions.js
@@ -11,15 +11,6 @@ const isPalindrome = (string) => {
   return normalized === reversed;
 };
 
-console.log(isStringLengthValid('проверяемая строка', 20)); // true
-console.log(isStringLengthValid('проверяемая строка', 18)); // true
-console.log(isStringLengthValid('проверяемая строка', 10)); // false
-
-console.log(isPalindrome('топот'));                      // true
-console.log(isPalindrome('ДовОд'));                      // true
-console.log(isPalindrome('Кекс'));                       // false
-console.log(isPalindrome('Лёша на полке клопа нашёл ')); // true
-
 
 const extractDigits = (value) => {
   const string = value.toString();
@@ -36,12 +27,3 @@ const extractDigits = (value) => {
   return parseInt(digits, 10);
 };
 
-console.log(extractDigits('2023 год'));            // 2023
-console.log(extractDigits('ECMAScript 2022'));     // 2022
-console.log(extractDigits('1 кефир, 0.5 батона')); // 105
-console.log(extractDigits('агент 007'));           // 7
-console.log(extractDigits('а я томат'));           // NaN
-
-console.log(extractDigits(2023)); // 2023
-console.log(extractDigits(-1));   // 1
-console.log(extractDigits(1.5));  // 15
